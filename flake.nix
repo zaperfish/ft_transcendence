e39ecd0199175bddff12a308c2c933cbb9538687
Zaperfish <105299514+zaperfish@users.noreply.github.com>
2026-05-03 15:40:12 +0200

    chore: bootstrap go backend (#1)
    * chore: add nix dev flake
    
    * chore: add minimal go backend
    
    * chore: add gitignore

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "ft_transcendence development flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs_24
+        go
+        gopls
+        just
+      ];
+
+      shellHook = ''
+        echo "entering development shell"
+      '';
+    };
+  };
+}

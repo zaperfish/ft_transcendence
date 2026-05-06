2142cde81db10d5007a1deffddc85cdc353e5e97
zaperfish <zaperfish@gmail.com>
2026-05-06 22:03:18 +0200

    fix: api url when developing locally

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    const apiBase = process.env.LOCAL_API_BASE_URL;
+
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${apiBase}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

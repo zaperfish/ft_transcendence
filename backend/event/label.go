2648a5eed1002920b198d79a76c9dd89209dfa70
Zaperfish <105299514+zaperfish@users.noreply.github.com>
2026-05-17 21:44:07 +0200

    refactor: move labels api to separate file (#23)

---
+++ backend/event/label.go
@@ -0,0 +1,180 @@
+package event
+
+import (
+	// Std
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	// Extern
+	"github.com/danielgtaylor/huma/v2"
+	"gorm.io/gorm"
+)
+
+type LabelHandler struct {
+	db *gorm.DB
+}
+
+func RegisterLabelsApi(api huma.API, db *gorm.DB) {
+	h := LabelHandler{db: db}
+
+	db.AutoMigrate(&Label{})
+
+	// Register POST /labels
+	huma.Register(api, huma.Operation{
+		OperationID:   "create-label",
+		Method:        http.MethodPost,
+		Path:          "/api/labels",
+		Summary:       "Create a label",
+		Tags:          []string{"Labels"},
+		DefaultStatus: http.StatusCreated,
+	}, h.HandlePostLabel)
+
+	// Register GET /labels/{id}
+	huma.Register(api, huma.Operation{
+		OperationID:   "get-label",
+		Method:        http.MethodGet,
+		Path:          "/api/labels/{id}",
+		Summary:       "Get a label by ID",
+		Tags:          []string{"Labels"},
+		DefaultStatus: http.StatusOK,
+	}, h.HandleGetLabel)
+
+	// Register GET /labels
+	huma.Register(api, huma.Operation{
+		OperationID:   "get-labels",
+		Method:        http.MethodGet,
+		Path:          "/api/labels",
+		Summary:       "Get a list of labels",
+		Tags:          []string{"Labels"},
+		DefaultStatus: http.StatusOK,
+	}, h.HandleGetLabels)
+
+	// Register DELETE /labels/{id}
+	huma.Register(api, huma.Operation{
+		OperationID:   "delete-label",
+		Method:        http.MethodDelete,
+		Path:          "/api/labels/{id}",
+		Summary:       "Delete a a label",
+		Tags:          []string{"Labels"},
+		DefaultStatus: http.StatusOK,
+	}, h.HandleDeleteLabel)
+}
+
+type Label struct {
+	gorm.Model
+
+	// Core
+	Name string `gorm:"not null;uniqueIndex;check:length(name) >= 2"`
+}
+
+type LabelDTO struct {
+	ID        uint      `json:"id" doc:"ID of the event"`
+	CreatedAt time.Time `json:"created_at" doc:"Time the event got created"`
+	UpdatedAt time.Time `json:"updated_at" doc:"Time the event got updated"`
+	Name      string    `json:"name" doc:"Name of the label"`
+}
+
+type LabelListDTO struct {
+	Data     []LabelDTO `json:"data"`
+	Page     int        `json:"page"`
+	PageSize int        `json:"page_size"`
+	Total    int        `json:"total"`
+}
+
+type CreateLabelDTO struct {
+	Name string `json:"name" minLenght:"2" maxLength:"15" example:"Go" doc:"Name of the label"`
+}
+
+type CreateLabelInput struct {
+	Body CreateLabelDTO
+}
+
+type GetLabelsInput struct {
+	Page     int `query:"page" minimum:"1" default:"1" doc:"Filter by page"`
+	PageSize int `query:"page_size" minimum:"1" default:"10" doc:"Page size"`
+}
+
+type LabelOutput struct {
+	Body LabelDTO
+}
+
+type LabelsOutput struct {
+	Body LabelListDTO
+}
+
+func (l *Label) toDTO() LabelDTO {
+	labelDTO := LabelDTO{
+		Name:      l.Name,
+		ID:        l.ID,
+		CreatedAt: l.CreatedAt,
+		UpdatedAt: l.UpdatedAt,
+	}
+
+	return labelDTO
+}
+
+func (h *LabelHandler) HandlePostLabel(ctx context.Context, input *CreateLabelInput) (*LabelOutput, error) {
+	label := Label{
+		Name: input.Body.Name,
+	}
+
+	err := gorm.G[Label](h.db.Debug()).Create(ctx, &label)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create label: %w", err)
+	}
+
+	return &LabelOutput{Body: label.toDTO()}, nil
+}
+
+func (h *LabelHandler) HandleGetLabel(ctx context.Context, input *GetEventInput) (*LabelOutput, error) {
+	label, err := gorm.G[Label](h.db.Debug()).Where("id = ?", input.ID).First(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get label: %w", err)
+	}
+
+	return &LabelOutput{Body: label.toDTO()}, nil
+}
+
+func (h *LabelHandler) HandleGetLabels(ctx context.Context, input *GetLabelsInput) (*LabelsOutput, error) {
+	base := gorm.G[Label](h.db.Debug())
+	q := base.Limit(input.PageSize)
+	offset := (input.Page - 1) * input.PageSize
+	q = q.Offset(offset)
+
+	labels, err := q.Find(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get events: %w", err)
+	}
+
+	total := len(labels)
+	labelsDTO := make([]LabelDTO, total)
+	for i, label := range labels {
+		labelsDTO[i] = label.toDTO()
+	}
+
+	labelsOutput := &LabelsOutput{
+		Body: LabelListDTO{
+			Data:     labelsDTO,
+			Page:     input.Page,
+			PageSize: input.PageSize,
+			Total:    total,
+		},
+	}
+
+	return labelsOutput, nil
+}
+
+func (h *LabelHandler) HandleDeleteLabel(ctx context.Context, input *GetEventInput) (*struct{}, error) {
+	rows, err := gorm.G[Label](h.db.Debug()).Where("id = ?", input.ID).Delete(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete label: %w", err)
+	}
+
+	if rows == 0 {
+		return nil, fmt.Errorf("failed to delete label: record not found")
+	}
+
+	return nil, nil
+}
--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,35 @@
 package main
 
 import (
-	"context"
-	"ft_transcendence/backend/db"
-	"ft_transcendence/backend/event"
-	"ft_transcendence/backend/user"
-	// "encoding/json"
-	"fmt"
+	// Std
 	"log"
 	"net/http"
 	"os"
 
+	// Internal
+	"ft_transcendence/backend/db"
+	"ft_transcendence/backend/event"
+	"ft_transcendence/backend/user"
+
+	// External
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humachi"
-	// "github.com/danielgtaylor/huma/v2/adapters/humago"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
-	"gorm.io/gorm"
 )
 
-type Handler struct {
-	db *gorm.DB
+func startServer(r *chi.Mux) {
+	port, ok := os.LookupEnv("PORT")
+	if !ok || port == "" {
+		port = "4000"
+	}
+
+	log.Println("Listening on :" + port + "...")
+	err := http.ListenAndServe(":"+port, r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
@@ -36,70 +44,22 @@ func main() {
 		log.Println("Backend is in container")
 	}
 
-	db, err := db.ConnectDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	config := huma.DefaultConfig("ft_transcendence api", "0.1.0")
 	config.DocsRenderer = huma.DocsRendererScalar
-	// config.SchemasPath = ""
-	// config.OpenAPIPath = "/api/openapi.json"
-	// config.DocsPath = ""
 
 	api := humachi.New(r, config)
 
-	h := &Handler{db: db}
-
-	user.RegisterApi(api, db)
-	event.RegisterApi(api, db)
-	huma.Get(api, "/api/postgres-version", h.HandlePostgresVersion)
-	huma.Get(api, "/api/greeting/{name}", h.HandleGreeting)
-
-	port, ok := os.LookupEnv("PORT")
-	if !ok || port == "" {
-		port = "4000"
-	}
-
-	log.Println("Listening on :" + port + "...")
-	err = http.ListenAndServe(":"+port, r)
+	db, err := db.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-type PostgresVersionOutput struct {
-	Body struct {
-		Version string `json:"version"`
-	}
-}
+	user.RegisterApi(api, db)
+	event.RegisterEventsApi(api, db)
+	event.RegisterLabelsApi(api, db)
 
-func (h *Handler) HandlePostgresVersion(ctx context.Context, input *struct{}) (*PostgresVersionOutput, error) {
-	var version string
-	err := h.db.Raw("SELECT version()").Scan(&version).Error
-	if err != nil {
-		return nil, huma.Error500InternalServerError("query failed")
-	}
-	resp := &PostgresVersionOutput{}
-	resp.Body.Version = version
-	return resp, nil
-}
-
-type GreetingInput struct {
-	Name string `path:"name" maxLength:"30" example:"Max" doc:"Name to greet"`
-}
-
-type GreetingOutput struct {
-	Body struct {
-		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
-	}
-}
-
-func (h *Handler) HandleGreeting(ctx context.Context, input *GreetingInput) (*GreetingOutput, error) {
-	resp := &GreetingOutput{}
-	resp.Body.Message = fmt.Sprintf("Hello %s", input.Name)
-	return resp, nil
+	startServer(r)
 }

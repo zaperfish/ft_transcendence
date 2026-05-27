b1abb7a486547a487081a149101c5b455165f23c
yingzhan25 <3146589226@qq.com>
2026-05-27 15:58:26 +0200

    chore: add comments for endpoint change

--- frontend/lib/api/user.ts
+++ frontend/lib/api/user.ts
@@ -1,6 +1,10 @@
 import { request } from '@/lib/api/client';
 import type { User } from '@/types/user';
 
+/**
+ * Restore User information and authtication for refresh and protected pages
+ * @returns Promise of User
+ */
 export async function getCurrentUser(): Promise<User> {
 	return request<User>('/api/me');
 }

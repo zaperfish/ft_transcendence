c6d4b9ac1d4ad435d4297a0537e490db39cca16f
david <75440456+maricalmer@users.noreply.github.com>
2026-05-30 23:38:51 +0200

    feat(chat): add initial backend chat package skeleton (#52)
    * feat(chat): add websocket dependency and chat package
    * docs(chat): add documention for hub and room map

---
+++ backend/chat/hub.go
@@ -0,0 +1,12 @@
+package chat
+
+type Hub struct {
+    // rooms stores active chat rooms by event ID
+    rooms map[uint]*Room
+}
+
+func NewHub() *Hub {
+    return &Hub{
+        rooms: make(map[uint]*Room),
+    }
+}

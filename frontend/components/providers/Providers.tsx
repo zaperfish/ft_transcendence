9342b09f3867dafc555c1c042fe4a35a06e7f8af
yingzhan25 <3146589226@qq.com>
2026-06-04 12:49:27 +0200

    feat: add queryclient to providers

---
+++ frontend/components/providers/Providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { AuthProvider } from "@/lib/context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Avoid instantiate whenever children are rerendered and lose cache
+const queryClient = new QueryClient();
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<AuthProvider>
+				{children}
+			</AuthProvider>
+		</QueryClientProvider>
+	);
+}

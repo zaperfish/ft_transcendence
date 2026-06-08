1174edae1dbbc2e56cc901c08b5204393f1d4019
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2026-06-08 11:13:41 +0000

    feat(api): add /api/me/join/{event_id} endpoint

--- backend/me/handler.go
+++ backend/me/handler.go
@@ -8,6 +8,7 @@ import (
     // Internal
 	"ft_transcendence/backend/auth"
 	"ft_transcendence/backend/errs"
+	"ft_transcendence/backend/event"
 	"ft_transcendence/backend/user"
 
     // External
@@ -19,10 +20,12 @@ import (
 
 type MeHandler struct {
 	su user.UserService
+	se event.EventService
 }
 
 func NewHandler(db *gorm.DB) MeHandler {
-	return MeHandler{su: user.NewUserService(user.NewUserRepository(db))}
+	return MeHandler{su: user.NewUserService(user.NewUserRepository(db)),
+					 se: event.NewEventService(event.NewEventRepository(db), db)}
 }
 
 func (h *MeHandler) handleGetMe(ctx context.Context, in *struct{}) (*user.UserOutput, error) {
@@ -99,8 +102,59 @@ func (h *MeHandler) handleDeleteMe(ctx context.Context, in *struct{}) (*struct{}
 	}
     return nil, nil
 }
+
+// join event
+
+type JoinEventInput struct {
+	EventID string `path:"id" doc:"Event ID"`
+}
+
+func (h *MeHandler) handleJoinEventMe(ctx context.Context, input *JoinEventInput) (*event.AddParticipantOutput, error) {
+	sub, err := auth.ClaimFromCtx(ctx)
+	if err != nil {
+		return nil, huma.Error404NotFound(errs.ErrNotFound.Error())
+	}
+
+	err = h.se.AddParticipant(ctx, input.EventID, sub)
+	if err != nil {
+		return nil, huma.Error500InternalServerError("", err)
+	}
+
+	return &event.AddParticipantOutput{}, nil
+}
+
+// create event
+
+// func (h *MeHandler) handleCreateEventMe(ctx context.Context,  input *event.CreateEventInput) (*event.CreateEventOutput, error) {
+// 	id, err := auth.UidFromCtx(ctx)
+// 	if err != nil {
+// 		return nil, huma.Error404NotFound(errs.ErrNotFound.Error())
+// 	}
 //
-// func (h *Handler) handleEventsMe(ctx context.Context. in *struct{}) (*event.ListEventsOutput, error) {
+// 	event := event.Event{
+// 		Title:           input.Body.Title,
+// 		Description:     input.Body.Description,
+// 		StartTime:       input.Body.StartTime,
+// 		Duration:        input.Body.Duration,
+// 		LocationName:    input.Body.LocationName,
+// 		LocationAddress: input.Body.LocationAddress,
+// 		MaxCapacity:     input.Body.MaxCapacity,
+// 	}
+//
+// 	created, err := h.se.CreateEvent(ctx, &event)
+// 	if err != nil {
+// 		return nil, huma.Error500InternalServerError("handler: failed to create event", err)
+// 	}
+//
+// 	err := h.service.AddParticipant(ctx, input.EventID, userID)
+// 	if err != nil {
+// 		return nil, huma.Error500InternalServerError("", err)
+// 	}
+//
+// }
+
+//
+// func (h *Handler) handleEventsMe(ctx context.Context, in *struct{}) (*event.ListEventsOutput, error) {
 // 	id, err := auth.UidFromCtx(ctx)
 // 	if err != nil {
 // 		return nil, huma.Error404NotFound(errs.ErrNotFound.Error())

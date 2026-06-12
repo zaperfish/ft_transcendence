c78bac206250dc5e490480d22ac62855efc9eed9
yingzhan25 <3146589226@qq.com>
2026-06-12 15:01:27 +0200

    feat: add api functions for detail page

--- frontend/lib/api/events.ts
+++ frontend/lib/api/events.ts
@@ -1,4 +1,5 @@
 import type { CreateEventRequest, EventEntity, EventsResponse } from "@/types/event";
+import type { User } from "@/types/user";
 import { request } from '@/lib/api/client';
 
 // API functions used for home page
@@ -29,7 +30,6 @@ export async function joinEvent(eventId: number): Promise<void> {
 	});
 }
 
-
 // API functions used for event list page
 // These two endpoints should be negotiated with backend
 // Dont have to catch errors because React Query will handle automatically
@@ -39,4 +39,54 @@ export async function getAttendingEvents(): Promise<EventEntity[]> {
 
 export async function getHostingEvents(): Promise<EventEntity[]> {
 	return request<EventEntity[]>('/api/events/hosting');
+}
+
+// API functions used for event detail page
+// Get single event by Id
+export async function getEventById(id: number): Promise<EventEntity> {
+	return request<EventEntity>(`/api/events/${id}`);
+}
+
+// Get list of participants of an event
+export async function getEventParticipants(id: number): Promise<User[]> {
+	const response = await request<{ data: User[] }>(
+		`/api/events/${id}/participants`
+	);
+	return response.data;
+}
+
+// Update event information (Admin-only)
+export async function updateEvent(
+	id: number,
+	data: Partial<CreateEventRequest>
+): Promise<EventEntity> {
+	return request<EventEntity>(`/api/events/${id}`, {
+		method: "PATCH",
+		headers: { "Content-type": "application/json" },
+		body: JSON.stringify(data),
+	});
+}
+
+// Delete event (Admin-only)
+export async function deleteEvent(id: number): Promise<void> {
+	return request<void>(`/api/events/${id}`, {
+		method: "DELETE",
+	});
+}
+
+// Remove participants (Admin-only)
+export async function removeParticipant(
+	eventId: number,
+	userId: number
+): Promise<void> {
+	return request<void>(`/api/events/${eventId}/participants/${userId}`, {
+		method: "DELETE",
+	});
+}
+
+// User unregisters event
+export async function leaveEvent(id: number): Promise<void> {
+	return request<void>(`/api/me/leave/${id}`, {
+		method: "DELETE",
+	});
 }

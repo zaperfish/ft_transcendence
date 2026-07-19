db546e446441baa202d88aa0a803877374a193ca
zaperfish <zaperfish@gmail.com>
2026-07-19 12:17:10 +0200

    fix: change 500 error to 404 not found

--- backend/apikey/apikey.go
+++ backend/apikey/apikey.go
@@ -55,7 +55,7 @@ func StoreApiKey(key string, ctx context.Context, db *gorm.DB) error {
 
 	err := gorm.G[GormApiKeyModel](db.Debug()).Create(ctx, &model)
 	if err != nil {
-		return err
+		return huma.Error409Conflict("api key exists")
 	}
 
 	return nil
@@ -127,7 +127,7 @@ func (h *ApiKeyHandler) DeleteApiKeyHandler(ctx context.Context, input *DeleteAp
 	}
 
 	if rows == 0 {
-		return nil, fmt.Errorf("api key not found")
+		return nil, huma.Error404NotFound("record not found")
 	}
 
 	return &DeleteApiKeyOutput{}, nil
